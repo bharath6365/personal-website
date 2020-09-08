b36a72de55b6f742f46516a4b9139f9fb6f7ca99
bharathkumar-chandrasekaran <bharath6365@gmail.com>
2020-09-08 14:43:15 +0530

    Add hamburger nav.

--- components/nav.js
+++ components/nav.js
@@ -1,33 +1,61 @@
 import Link from 'next/link';
+import { useState, Fragment } from 'react';
 import styled from 'styled-components';
+import { Spin as Hamburger } from 'hamburger-react'
 
 import GlobalConstants from '../styles/Global-Constants';
+import GlobalTheme from '../styles/Global-Theme';
 
-const Nav = ({ settings }) => (
-  <NavStyled>
-    <NavWrapper className="l-page">
-      <Link href="/">
-        <LogoWrapper>
-          <img src="/images/bee.png" />
-       </LogoWrapper>
-      </Link>
-      
+const Nav = ({ settings }) => {
+  const [ isOpen, setOpen ] = useState(false);
+  return (
+    <Fragment>
+      <NavStyled>
+        <NavWrapper className="l-page">
+          <Link href="/">
+            <StyledLogoWrapper>
+              <img src="/images/bee.png" />
+            </StyledLogoWrapper>
+          </Link>
 
-      <ContentWrapper>
-        {settings &&
-          settings.content.main_navi.map((navItem, index) => {
-            return (
-              <Link href={navItem.link.url} key={navItem._uid}>
-                <a>{navItem.name}</a>
-              </Link>
-            );
-          })}
-      </ContentWrapper>
+          <ContentWrapper>
+            {settings &&
+              settings.content.main_navi.map((navItem, index) => {
+                return (
+                  <Link href={navItem.link.url} key={navItem._uid}>
+                    <a>{navItem.name}</a>
+                  </Link>
+                );
+              })}
+          </ContentWrapper>
+        </NavWrapper>
+      </NavStyled>
 
-  
-    </NavWrapper>
-  </NavStyled>
-);
+      <NavStyledMobile>
+        <Hamburger toggled={isOpen} toggle={setOpen} />
+      </NavStyledMobile>
+
+      <StyledMobileNav className={isOpen ? 'active' : ''}>
+        <Link href="/">
+          <StyledLogoWrapper>
+            <img src="/images/bee.png" />
+          </StyledLogoWrapper>
+        </Link>
+
+        <StyledMobileLinks>
+          {settings &&
+            settings.content.main_navi.map((navItem, index) => {
+              return (
+                <Link href={navItem.link.url} key={navItem._uid}>
+                  <a>{navItem.name}</a>
+                </Link>
+              );
+            })}
+        </StyledMobileLinks>
+      </StyledMobileNav>
+    </Fragment>
+  );
+};
 
 export default Nav;
 
@@ -38,15 +66,17 @@ const NavStyled = styled.nav`
   top: 0;
   left: 0;
   width: 100%;
-  height: ${GlobalConstants.navHeight + "px"};
+  height: ${GlobalConstants.navHeight + 'px'};
   z-index: 1000;
   display: flex;
-
-  
 
   a {
     color: white;
     text-decoration: none;
+  }
+
+  @media ${GlobalConstants.mobileMediaQuery} {
+    display: none;
   }
 `;
 
@@ -56,9 +86,14 @@ const NavWrapper = styled.div`
   align-items: center;
 `;
 
-const LogoWrapper = styled.div`
+const StyledLogoWrapper = styled.div`
   flex: 1;
   cursor: pointer;
+
+  @media ${GlobalConstants.mobileMediaQuery} {
+    text-align: center;
+    margin-bottom: 20px;
+  }
 
   img {
     width: 60px;
@@ -67,7 +102,6 @@ const LogoWrapper = styled.div`
 `;
 
 const ContentWrapper = styled.div`
-
   &:hover a {
     filter: blur(1px);
   }
@@ -101,8 +135,69 @@ const ContentWrapper = styled.div`
   a:hover:before {
     transform: scaleX(1);
   }
+`;
+
+const NavStyledMobile = styled.div`
+  display: none;
 
   @media ${GlobalConstants.mobileMediaQuery} {
-    display: none;
+    display: block;
+    position: fixed;
+    top: 10px;
+    right: 15px;
+    z-index: 10000;
+    transform: skewX(-10deg);
+    background: ${GlobalTheme.primary};
   }
 `;
+
+const StyledMobileNav = styled.div`
+  position: fixed;
+  width: 55vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  transform: translateX(-200%);
+  z-index: 1000;
+  background: rgb(45 14 36 / 0.95);
+  border-right: 2px solid rgb(226, 226, 226);
+  transition: transform 1s cubic-bezier(.42,.97,.52,1.49);
+
+  &.active {
+    transform: translateX(0);
+
+  }
+
+  a {
+    position: relative;
+    font-size: 1.375rem;
+    padding: 10px 0;
+    display: block;
+    color: white;
+    text-decoration: none;
+    text-align: center;
+
+    &:before {
+      position: absolute;
+      content: '';
+      width: 100%;
+      height: 100%;
+      background: ${props => props.theme.primary};
+      left: 0;
+      top: 0;
+      transform: translateX(-1000%);
+      transition: transform 0.4s ease;
+      z-index: -1;
+    }
+
+    &:hover {
+      color: black;
+
+      &:before {
+        transform: translateX(0);
+      }
+    }
+  }
+`;
+
+const StyledMobileLinks = styled.div``;
