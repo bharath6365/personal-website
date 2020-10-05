6d5be53b2c3113d4db1d8e292a236c9ee6332c48
bharathkumar-chandrasekaran <bharath6365@gmail.com>
2020-10-05 11:58:58 +0530

    Add og image for website

--- pages/_document.js
+++ pages/_document.js
@@ -40,6 +40,14 @@ export default class MyDocument extends Document {
 
           <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
 
+          <meta property="og:url" content="bharath.work" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Bharath: Full Stack Developer" />
+          <meta property="og:description" content="Senior Software Engineer with 5 years of experience currently working at Freshworks." />
+
+          <meta property="og:image" content="https://bharath.work/images/og.png" />
+          <meta name="og:image" content="https://bharath.work/images/og.png" />
+
 
 
         </Head>
