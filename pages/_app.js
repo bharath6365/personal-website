8d321df30f2bdf30be3ef235aa7a5f72736da095
Bharath Kumar <bharath6365@gmail.com>
2024-04-27 17:42:38 +0530

    feat: add ms script

--- pages/_app.js
+++ pages/_app.js
@@ -25,12 +25,20 @@ class MyApp extends App {
   }
   render() {
     const { Component, pageProps } = this.props;
+    const script = `
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "m356ptg4m2");
+    `;
     return (
       <ThemeProvider theme={GlobalTheme}>
         <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+          <script dangerouslySetInnerHTML={{__html: script}} />
         </Head>
         <Container>
           <Component {...pageProps} />
